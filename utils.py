0048220f2beebf8cf929dd93ffeeac78dfd062f4
sonalif <sonali.fotedar@gmail.com>
2020-01-11 15:27:32 +0100

    flush print statements to log file

--- utils.py
+++ utils.py
@@ -11,11 +11,6 @@ from torchvision import transforms
 from torchvision.utils import save_image
 
 
-def write_log(filename, line):
-	with open(filename, 'w', encoding='utf-8') as f:
-		f.writelines(line)
-	f.close()
-
 
 def to_var(x):
 	if torch.cuda.is_available():
